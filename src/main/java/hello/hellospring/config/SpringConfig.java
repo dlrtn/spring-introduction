2cb7425e88aa6a14ac46b00e4eeb533b483f88a6
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-11-04 01:33:40 +0900

    불필요한 공백 삭제

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -19,6 +19,4 @@ public class SpringConfig {
         return new MemoryMemberRepository();
     }
 
-
-
 }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
+
     private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {

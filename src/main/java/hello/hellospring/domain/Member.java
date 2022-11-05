a3da6b81f25a81c70d291382519308cbe5482371
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-11-05 21:00:02 +0900

    JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,14 @@
 package hello.hellospring.domain;
 
-public class Member {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -20,5 +27,4 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
-
 }
